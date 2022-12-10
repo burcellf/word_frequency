e03fb87e836b7335347aca600759d3c9e09e9d1e
Jon Woods <98776428+burcellf@users.noreply.github.com>
2022-12-10 12:19:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ This is a program that finds the most common words used on a website page by:
             - the programm searches through the site body for text,
             - strips the text of its html formatting,
             - creates individual words via text parsing
-            - removes low-impact words (such as 'and', 'to', 'the', etc),
+            - removes unnecessary words (such as 'and', 'to', 'the', etc),
             
     3) Organizing data
-            - counts the frequency each (remaining) word occured on the page
-            - orders the words from most frequent to least frequent
+            - counts the occurrence of each word
+            - orders words from most frequent to least frequent
 
     4) Presenting data
-            - prints the words and their frequency in decending order
+            - prints words and their frequency in decending order
             - (optional) only prints words that occured above a given frequency (default = 3)
 
     5) Saving data
@@ -28,9 +28,9 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-# Go to this url, and get the .text
-source = requests.get('https://www.kuow.org/careers/Data-Analyst-Temporary').text
-# Store url.text data in this var
+# Enter the url you want to use, and get the .text
+source = requests.get('url').text
+# Store a reference to the url.text data
 soup = BeautifulSoup(source, 'lxml')
 
 # Find the word 'article' in the html code
@@ -39,9 +39,9 @@ article = soup.find('article')
 interesting = article.find('div', class_='body-content').text
 
 
-# Use this if you want to save data as a .csv
+# Uncomment this section to save the data as a .csv
 """
-with open('job_description.csv', 'w', encoding='utf8', newline='') as f:
+with open('file_name.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = csv.writer(f)
 """
 
@@ -105,10 +105,8 @@ for str in interesting.split():
 # Return the max word count as an int
 max_num = max(word_dict, key=word_dict.get)
 
-
-# Convert to list to order data easily
+# Create a list to easily organize dictionary data
 word_list = []
-
 
 for i in range(word_dict[max_num] + 1):
     for key, val in word_dict.items():
@@ -119,19 +117,21 @@ for i in range(word_dict[max_num] + 1):
 sorted_word_list = sorted(word_list, reverse=True)
 
 
-# Print the entire list:
+# Uncomment to print the entire list:
 """
 print(f'\n\nword_list: {sorted_word_list}\n\n')
 """
 
 # Or
 
-# Only print words above a specified freq (3)
+# Only print words that occured more than n times
+n = 3
+
 print('\n\n')
 print(f'\tFreq  |  Word')
 print(f'—' * 30)
 for i in range(len(sorted_word_list)):
-    if sorted_word_list[i][0] > 3:
+    if sorted_word_list[i][0] > n:
         if sorted_word_list[i][0] >= 100:
             print(f'\t {sorted_word_list[i][0]}: |  {sorted_word_list[i][1]}')
         elif 10 <= sorted_word_list[i][0] < 100:
